8213844cf99d48ddea5876f4e9a7dc18a34b4f87
Pierre EVEN <spam@evenpierre.fr>
2025-04-13 14:44:06 +0200

    fixed download url

--- scripts/download_run.sh
+++ scripts/download_run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-wget -O /opt/bidibip/bidibip.zip https://github.com/Unreal-Engine-FR/Bidibip/releases/download/latest/bidibip_linux.zip
+wget -O /opt/bidibip/bidibip.zip https://github.com/Unreal-Engine-FR/Bidibip/releases/latest/download/bidibip_linux.zip
 unzip /opt/bidibip/bidibip.zip -d /opt/bidibip
 rm /opt/bidibip/bidibip.zip
 mv /opt/bidibip/bidibip/bidibip-core /opt/bidibip/bidibip_core
