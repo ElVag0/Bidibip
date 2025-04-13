20f524cd5f8fe573b19d09b35ed7c726c7b57607
Pierre EVEN <spam@evenpierre.fr>
2025-04-13 14:30:02 +0200

    fixed download script that was using an outdated bot version

--- scripts/download_run.sh
+++ scripts/download_run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-wget -O /opt/bidibip/bidibip.zip https://github.com/Unreal-Engine-FR/Bidibip/releases/download/v4.0.8/bidibip_linux.zip
+wget -O /opt/bidibip/bidibip.zip https://github.com/Unreal-Engine-FR/Bidibip/releases/download/latest/bidibip_linux.zip
 unzip /opt/bidibip/bidibip.zip -d /opt/bidibip
 rm /opt/bidibip/bidibip.zip
 mv /opt/bidibip/bidibip/bidibip-core /opt/bidibip/bidibip_core
