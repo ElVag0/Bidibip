86c687722c6afdfce6cb5a502a50defbd4ea9832
Pierre EVEN <spam@evenpierre.fr>
2025-04-13 14:59:30 +0200

    fixed install script

--- scripts/download_run.sh
+++ scripts/download_run.sh
@@ -3,7 +3,7 @@
 wget -O /opt/bidibip/bidibip.zip https://github.com/Unreal-Engine-FR/Bidibip/releases/latest/download/bidibip_linux.zip
 unzip /opt/bidibip/bidibip.zip -d /opt/bidibip
 rm /opt/bidibip/bidibip.zip
-mv /opt/bidibip/bidibip/bidibip-core /opt/bidibip/bidibip_core
+mv /opt/bidibip/bidibip/bidibip /opt/bidibip/bidibip_exe
 rmdir /opt/bidibip/bidibip/
-chmod u+x /opt/bidibip/bidibip_core
-/opt/bidibip/bidibip_core /saved/config.json
+chmod u+x /opt/bidibip/bidibip_exe
+/opt/bidibip/bidibip_exe /saved/config.json
